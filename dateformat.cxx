134bee3297f35ac39c7234b120659927f9603da0
madhav-datt <madhav.datt@hotmail.com>
2016-03-01 14:07:00 +0530

    format semantics for dates

--- dateformat.cxx
+++ dateformat.cxx
@@ -53,28 +53,43 @@ using namespace std;
 //         strcpy (dateFormat, "0");
 // }
 
-// The parameters below takes designated values with the specified format semantics
+/**
+ * Format semantics for date, month, year formatter
+ *
+ * dateFormat:
+ * 0: No date. This is valid for output only.
+ * "d": single digit date in one digit, double digit date in two digits (7, 23)
+ * "dd": all dates in two digits with single digit dates with leading 0 (07, 23)
+ *
+ * monthFormat:
+ * 0: each month in its full name (February, November). This is valid for output only.
+ * "m": single digit month in one digit, double digit month in two digits (2, 11)
+ * "mm": all months in two digits with single digit months with leading 0 (02, 11)
+ * "mmm": each month with first three letters of its name (Feb, Nov). This is valid for output only.
+ *
+ * yearFormat:
+ * 0: No year. This is valid for output only.
+ * "yy": year in last two digits (1961 as 61, 2016 as 16)
+ * "yyyy": year in four digits (1961 as 1961, 2016 as 2016)
+ */
 
-// dateFormat:
-// 0: No date. This is valid for output only.
-// "d": single digit date in one digit, double digit date in two digits (7, 23)
-// "dd": all dates in two digits with single digit dates with leading 0 (07, 23)
-// monthFormat:
-// 0: each month in its full name (February, November). This is valid for output only.
-// "m": single digit month in one digit, double digit month in two digits (2, 11)
-// "mm": all months in two digits with single digit months with leading 0 (02, 11)
-// "mmm": each month with first three letters of its name (Feb, Nov). This is valid for output only.
-// yearFormat:
-// 0: No year. This is valid for output only.
-// "yy": year in last two digits (1961 as 61, 2016 as 16)
-// "yyyy": year in four digits (1961 as 1961, 2016 as 2016)
+/**
+ * Constructor for DateFormat Class
+ * Convert passed to dateFormat, monthFormat, yearFormat to accepted formatting
+ */
 DateFormat::DateFormat (const char* dateFormat, const char* monthFormat, const char* yearFormat)
 {
-
+    // Date, Month and Year string formatters
+    formatter_date (dateFormat, DateFormat::dateFormat);
+    formatter_month (monthFormat, DateFormat::monthFormat);
+    formatter_year (yearFormat, DateFormat::yearFormat);
 }
 
-// Single C-string format where dateFormat, monthFormat, and yearFormat are separated by ’-’
-// Example: "dd-mmm-yy", "d-mm-yyyy", etc
+/**
+ * Single C-string format Constructor for DateFormat Class
+ * dateFormat, monthFormat, and yearFormat separated by ’-’
+ * "dd-mmm-yy", "d-mm-yyyy", etc. formats
+ */
 DateFormat::DateFormat (const char* format)
 {
     // Take '-' as delimiter to break string
@@ -82,7 +97,10 @@ DateFormat::DateFormat (const char* format)
     char* tmp_month = strtok (NULL,"-");
     char* tmp_year = strtok (NULL,"-");
 
-
+    // Date, Month and Year string formatters
+    formatter_date (tmp_date, dateFormat);
+    formatter_month (tmp_month, monthFormat);
+    formatter_year (tmp_year, yearFormat);
 }
 
 /**
@@ -125,7 +143,9 @@ DateFormat::DateFormat ()
     strcat (yearFormat, to_string (year % 100));
 }
 
-// Destructor for the DateFormat class
+/**
+ * Destructor for the DateFormat class
+ */
 DateFormat::~DateFormat ()
 {
     delete[] dateFormat;
