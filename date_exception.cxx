fb9a16c5bbe75749f601637df0fbeaef373a8409
madhav-datt <madhav.datt@hotmail.com>
2016-03-02 18:45:43 +0530

    implement exception class constructors

--- date_exception.cxx
+++ date_exception.cxx
@@ -17,8 +17,46 @@
 
 // Implementation of Constructors
 
-// Base Exception Class
-Exception (char* function, uint32_t line):
-    
+/**
+ * Base Exception Class
+ */
+Exception::Exception (char* function, uint32_t line) :
+function_name (function), line_num (line)
+{
+}
+
+/**
+ * Invalid argument exception class
+ * One or more of the arguments d or m is/are invalid (27, 13, 13)
+ */
+invalid_argument::invalid_argument (char* function, uint32_t line, uint32_t date, uint32_t month) :
+Exception (function, line), invalid_argument::date (date), invalid_argument::month (month)
+{
+}
+
+/**
+ * domain_error exception class
+ * (d, m, y) as a triplet does not define a valid date (30, 2, 61)
+ */
+domain_error::domain_error (char* function, uint32_t line, uint32_t date, uint32_t month, uint32_t year) :
+Exception (function, line), domain_error::date (date), domain_error::month (month), domain_error::year (year)
+{
+}
+
+/**
+ * out_of_range exception class
+ * Date is out of range (4, 7, 2053)
+ */
+out_of_range::out_of_range (char* function, uint32_t line, uint32_t year) :
+Exception (function, line), out_of_range::year (year)
+{
+}
+
+/**
+ * format_error exception class
+ * Incorrect format input provided
+ */
+format_error::format_error (char* function, uint32_t line, char* format) :
+Exception (function, line), format_error::format (format)
 {
 }
