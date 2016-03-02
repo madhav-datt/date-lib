9d4ac507630b4775ada04262cad2782db92dd24d
madhav-datt <madhav.datt@hotmail.com>
2016-03-02 19:35:05 +0530

    add test functions

---
+++ test_date.cxx
@@ -0,0 +1,21 @@
+/**
+ * Madhav Datt - 14CS30015
+ *
+ * Software Engineering Lab - Assignment 3
+ * Contains Main function
+ * Tests Date, DateFormat Classes and functionalities
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+#include "date.h"
+
+int main (void)
+{
+
+    return 0;
+}
